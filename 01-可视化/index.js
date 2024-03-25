9cae87ef54a19be0720b3b60252a3a0a80328c84
zhangshuang <zhangshuang@forensix.cn>
2024-03-25 22:23:39 +0800

    美化代码

--- 01-可视化/index.js
+++ 01-可视化/index.js
@@ -2,74 +2,83 @@ const audioCtx = new AudioContext();
 
 const btn_play_el = document.getElementById("btn_play");
 const btn_stop_el = document.getElementById("btn_stop");
+const btn_status_el = document.getElementById("btn_status");
 
 const canvasEl = document.getElementById("canvas");
 /** @type {HTMLCanvasElement} */
 const canvasCtx = canvasEl.getContext("2d");
 
-let isPlaying = false;
-let drawVisual;
-
-加载音频文件
-fetch("../static/demo.mp3")
-  .then((response) => {
-    console.log("加载完成", response);
-    btn_play_el.textContent = "加载完成";
-    return response.arrayBuffer();
-  })
-  .then((arrayBuffer) => {
-    console.log(arrayBuffer);
-    btn_play_el.textContent = "正在解码...";
-    return audioCtx.decodeAudioData(arrayBuffer);
-  });
-
-btn_play_el.addEventListener("click", () => {
+const init = () => {
+  // 加载音频文件
   btn_play_el.textContent = "正在加载音频...";
   fetch("../static/demo.mp3")
     .then((response) => {
-      console.log("加载完成", response);
       btn_play_el.textContent = "加载完成";
       return response.arrayBuffer();
     })
     .then((arrayBuffer) => {
-      console.log(arrayBuffer);
       btn_play_el.textContent = "正在解码...";
       return audioCtx.decodeAudioData(arrayBuffer);
     })
     .then((decodedBuffer) => {
-      console.log(decodedBuffer);
       btn_play_el.textContent = "解码完成, 配置音轨...";
-      // 创建音频源节点
-      const sourceNode = new AudioBufferSourceNode(audioCtx, {
-        buffer: decodedBuffer,
-      });
+      sourceNode = audioCtx.createBufferSource();
 
-      // 创建分析节点
-      const analyserNode = audioCtx.createAnalyser();
-      const array = new Uint8Array(analyserNode.frequencyBinCount); // 频率数组
-      console.log("**********", array);
+      sourceNode.buffer = decodedBuffer;
 
-      console.log(sourceNode, analyserNode);
+      analyserNode = audioCtx.createAnalyser();
+      // 设置快速傅里叶变换的大小
+      analyserNode.fftSize = 2048;
+
+      dataArray = new Uint8Array(analyserNode.frequencyBinCount)
+
       sourceNode.connect(analyserNode).connect(audioCtx.destination);
-      sourceNode.start(0);
-      btn_play_el.textContent = "播放中";
+      
+      btn_play_el.textContent = "play";
+    })
+}
 
-      const draw = () => {
-        drawVisual = requestAnimationFrame(draw);
-        analyserNode.getByteFrequencyData(array); // 获取频率数据
-        canvasCtx.clearRect(0, 0, canvasEl.width, canvasEl.height); // 清空画布
-        canvasCtx.strokeStyle = "#fff";
-        canvasCtx.lineWidth = 1;
-        canvasCtx.beginPath(); // 开始路径
-        canvasCtx.moveTo(0, canvasEl.height / 2);
-        for (let i = 0; i < array.length; i++) {
-          const value = array[i] || canvasEl.height / 2;
-          canvasCtx.lineTo(i, value);
-        }
-        canvasCtx.stroke(); // 结束路径
-      };
-      draw();
-    });
+// 音频源节点
+let sourceNode;
+// 分析节点
+let analyserNode;
+// 频率数据数组
+let dataArray;
+
+
+
+let drawVisual;
+const draw = () => {
+  console.log('在执行')
+  drawVisual = requestAnimationFrame(draw);
+  analyserNode.getByteFrequencyData(dataArray); // 获取频率数据
+  canvasCtx.clearRect(0, 0, canvasEl.width, canvasEl.height); // 清空画布
+  canvasCtx.strokeStyle = "#fff";
+
+  canvasCtx.lineWidth = 1;
+  canvasCtx.beginPath(); // 开始路径
+  canvasCtx.moveTo(0, canvasEl.height / 2);
+  for (let i = 0; i < dataArray.length; i++) {
+    const value = dataArray[i] / 255 * canvasEl.height;
+    canvasCtx.lineTo(i, value);
+  }
+  canvasCtx.stroke(); // 结束路径
+};
+
+
+btn_play_el.addEventListener("click", () => {
+  console.log(sourceNode, getIsPlaying())
+  // 
+  if (getIsPlaying()) {
+    sourceNode.stop();
+
+
+  } else {
+    const offset = audioCtx.currentTime % sourceNode.buffer.duration;
+    sourceNode.start(0, offset);
+  }
+  draw();
+
 });
 
 btn_stop_el.addEventListener("click", () => {
@@ -77,6 +86,26 @@ btn_stop_el.addEventListener("click", () => {
   cancelAnimationFrame(drawVisual);
   audioCtx.close();
 });
+
+btn_status_el.addEventListener("click", () => {
+  console.log(audioCtx)
+  console.log(sourceNode)
+  // 假设sourceNode是已经创建并播放的AudioBufferSourceNode对象
+  
+
+});
+
+init();
+
+const getIsPlaying = () => {
+  console.log(audioCtx.state )
+  if (audioCtx.state === 'running' && sourceNode && sourceNode.buffer) {
+    if (audioCtx.currentTime > 0 && audioCtx.currentTime < sourceNode.buffer.duration) {
+      return true;
+    }
+  }
+  return false;
+}
 
 // const audioEl = document.getElementById('audio')
 
